5115e0133df444fc334bea514c0eb377c6e7c8c0
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2026-06-05 00:23:54 +0530

    Version 1

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+
 const app = express();
+
 app.get("/", (req, res) => {
-    res.send("Version 1");
+  res.send("Version 1");
 });
+
 app.listen(3000, () => {
-    console.log("Server running");
-    console.log("Dev branch is running");
-    console.log("Server Test Version 1 is running");
+  console.log("Server running on port 3000");
 });
