996a345907c5ad6c4f83c2dafdf2a4810511ccc3
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2026-06-08 00:15:21 +0530

    Testing Jenkins auto deployment pipeline v2.2

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Version 1");
-  res.send("cicd working");
-  res.send("version 2.1 deployed");
+  res.send("cicd working | version 2.2 deployed");
 });
 
 app.listen(3000, () => {
